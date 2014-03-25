32c881a1b521d867282da21fbf0addf088385c6c
W. Hays <whays@nearity.com>
2014-03-25 18:15:13 -0400

    continued integration, complete set of tables, excluding bibliographic citations

--- chgis5-norm/chgis5-norm-ddl.sql
+++ chgis5-norm/chgis5-norm-ddl.sql
@@ -7,7 +7,7 @@ CREATE TABLE data_source {
   name     VARCHAR(32) NOT NULL,
   org      VARCHAR(64),
   uri      VARCHAR(1000),
-  note     TEXT
+  note     VARCHAR(1028)
 );
 
 INSERT INTO data_source VALUES ( 1, 'CHGIS', 'China Historical GIS', 'Fudan University, Center for Historical Geography', '', '');
@@ -39,7 +39,6 @@ CREATE TABLE feature_type (
   note                         VARCHAR(512)
 
   PRIMARY KEY  (feature_type_id)
-
 );
 
 -- look up table of 6 rules
@@ -48,7 +47,6 @@ CREATE TABLE date_rule (
   rule                         VARCHAR(512),
 
   PRIMARY KEY  (id)
-
 );
 
 
@@ -56,18 +54,21 @@ CREATE TABLE date_rule (
 CREATE TABLE placename (
 
   placename_id        INT auto_increment,        -- use for joins and internal purposes
-  order_id            INT NOT NULL,              -- the chgis primary identifier (not PK)
+                                                 -- dropped order_id
+  sys_id              VARCHAR(30) NOT NULL,              -- the chgis primary identifier (not PK)
 
-  data_src            ENUM ('CHGIS', 'CITAS', 'NIMA', 'RAS'),
   feature_type_id     INT NOT NULL,
+  data_src_id         INT NOT NULL,              -- FK, otherwise: ENUM ('CHGIS', 'CITAS', 'NIMA', 'RAS'),
+  src_note_id         INT,                       -- FK, can be NULL
 
-  lev_rank            CHAR(2) default NULL,      -- WHAT IS THIS?
+  level_rank          CHAR(2) default NULL,      -- prev: lev_rank ; administrative level assigned by FUDON
+
 
 -- temporal   -- prefer to require ISO 8601 as Calendar and formatting
               -- otherwise two fields:  calendar AND format
               -- or multiples in joined table
 
-  beg_chg_type        VARCHAR(60),                  -- are these lookups?, e.g. type 'time-slice'
+  beg_chg_type        VARCHAR(60),                  -- from FUDON, leave as is
   beg_chg_eng         VARCHAR(60),
   end_chg_type        VARCHAR(60),
   end_chg_eng         VARCHAR(60),
@@ -77,40 +78,38 @@ CREATE TABLE placename (
   end_yr              INT,                          -- ISO 8601 ?
   end_rule            INT,                          -- FK, change from char(1)
 
-
+  -- outer_begin                                       -- to show range of accuracy
+  -- outer_end
 
 -- spatial
 
-  --  sys_id         varchar(30) NOT NULL default '',   -- globally unique id
-                                                        -- is this used or exposed?
-  obj_type       ENUM('point', 'polygon', 'line'),      -- prev: varchar(10) default NULL
+  obj_type            ENUM('point', 'polygon', 'line'),      -- prev: varchar(10) default NULL
 
-  xy_type        varchar(20) default NULL,          -- consider ENUM or lookup
-  x_coord        varchar(30) default NULL,          -- why not float ??
-  y_coord        varchar(30) default NULL,
+  xy_type             ENUM('centroid', 'point', 'midpoint', 'point location', 'N/A'),   -- prev: VARCHAR(20),
+  x_coord             VARCHAR(30),          -- why not float ??
+  y_coord             VARCHAR(30),
 
-  geo_src        varchar(512),                             -- is this a note?
-  compiler       varchar(60) default NULL,
-  geocompiler    varchar(60) default NULL,
-  entry_date     varchar(12) default NULL,                -- why not a date type?
-  filename       VARCHAR(512),
-
-
--- present location and jurisdiction are types of placename spelling
-   -- unless they refer to a specific object of a certain type and can differ
-
+  geo_src             VARCHAR(512),                  -- is this a note?
+  compiler            VARCHAR(60),
+  geocompiler         VARCHAR(60),
+  entry_date          VARCHAR(12),                   -- why not a date type?
+  filename            VARCHAR(512),
 
 -- historical
 
 
 -- links
 
+PRIMARY KEY placename_id,
 
-
-FOREIGN KEY type_id REFERENCES(feature_type.feature_type_id)
+FOREIGN KEY type_id REFERENCES(feature_type.feature_type_id),
+FOREIGN KEY data_src_id REFERENCES(data_source.id),
+FOREIGN KEY src_note_id REFERENCES(src_note.feature_type_id),
+FOREIGN KEY type_id REFERENCES(feature_type.feature_type_id),
+-- FK for source note
+-- FKs for rules  FIXME
 
 );
-
 
 -- textual representation of a placename
 -- for type 'vernacular', require only one per placename
@@ -119,63 +118,78 @@ CREATE TABLE spelling (
 
   spelling_id        INT auto_increment,
   placename_id       INT NOT NULL,
-  spelling_type      ENUM('vernacular', 'exonym', 'transcription', 'present location',
-                          'present jurisdiction') NOT NULL,
+  spelling_type      ENUM('vernacular', 'exonym', 'transcription') NOT NULL,
   lang               VARCHAR(8),             -- when type transcription, lang of original
-  script             VARCHAR(16),
-  default_form       BOOLEAN NOT NULL DEFAULT false,
+  script             VARCHAR(16),            -- simplified, traditional
+--  default_form       BOOLEAN NOT NULL DEFAULT false,
+                                             -- can infer preferred form in app from other fields
 
-  written_form       VARCHAR(100),          -- i.e. the glyph, or text form
+  written_form       VARCHAR(128),           -- i.e. the glyph, or text form
 
 -- for type 'transcription'
-  transcription_of_id  INT,                  -- NULL for other types
-  transcription_system ENUM('py', 'wg', 'cyrillic', 'other'),                  --
+  transcription_of_id       INT,                  -- NULL for other types
+  transcription_system      ENUM('py', 'wg', 'cyrillic', 'other'),                  --
 
 -- how is 'alternate' (as in chgis3) expressed here?
-  alternate_of_id      INT,
+  alternate_of_id           INT,
 
 -- note field?  source info?  citation?  attestation
-  attested_by         VARCHAR(128),
+  attested_by               VARCHAR(128),
 
   PRIMARY KEY (spelling_id),
   FOREIGN KEY placename_id REFERENCES placename.placename_id,
   FOREIGN KEY transcription_of_id REFERENCES spelling.spelling_id,
   FOREIGN KEY alternate_of_id REFERENCES spelling.spelling_id
 
+);
+
+--
+CREATE TABLE temporal_annotation (
+  id                        INT auto_increment,
+  placename_name_id         INT NOT NULL,
+  temporal_type             ENUM('begin', 'end'),
+  calendar_standard         ENUM('ISO 8601', 'Chinese Lunar', 'Other'),  -- or lookup table
+  accuracy_rule_id          INT,             -- ?? type
+  attested_by               VARCHAR(128),
+  equivalent                VARCHAR(128),    -- what is this??
+  lang                      VARCHAR(8),      -- ISO code
+  note                      VARCHAR(512),
+
+  PRIMARY KEY (spelling_id),
+  FOREIGN KEY placename_id REFERENCES placename.placename_id,
+  FOREIGN KEY accuracy_rule_id REFERENCES(data_rule.id)       -- is this linked to the right table?
 
 );
 
-
-CREATE TABLE spatial_definition (
+-- well known text format
+-- this table cannot be so generalized due to the varying datatypes of the object
+-- solution:  create a different table for each type, in this case WKT
+--            this creates a problem for the system_ref table join ??
+CREATE TABLE wkt_definition (
   spatial_definition_id      INT auto_increment,
   placename_id               INT NOT NULL,
-  object_type_id             ENUM('point', 'centroid', 'polygon', 'line') NOT NULL,
-  definition_type            ENUM('coord', 'wkt', 'geo_json') NOT NULL,
+  object_type_id             ENUM('point', 'polygon', 'linestring', 'multipoint', 'multilinestring',
+                                  'multipolygon', 'geometrycollection') NOT NULL,
+  definition_type            ENUM('wkt') NOT NULL,        -- unnecessary ??
 
 --how to encode common point coordinates as in chgis3??
 --this seems like a standard gis thing
 
-  x_value                    DOUBLE,
-  x_bearing                  CHAR(1),
+--bounding box??
 
-  y_value                    DOUBLE,
-  y_bearing                  CHAR(1),
-
-  z_value                    DOUBLE,
-  z_bearing                  CHAR(1),
-
-  object_text_value          text,        -- what is the max here ?
+  object_text_value          TEXT,        -- are there size issues?
 
   PRIMARY KEY (spatial_definition_id),
+  FOREIGN KEY placename_id REFERENCES placename.placename_id
 );
 
-
--- if only one of these per parent, then combine
+-- external GIS citation, as in an index
+-- FIXME  what is the joined table ?
 CREATE TABLE spatial_system_ref (
   spatial_system_ref_id       INT auto_increment,
   spatial_definition_id       INT NOT NULL,
 
-  system_name                 VARCHAR(20),    -- lookup
+  system_name                 VARCHAR(20),    -- lookup, e.g. geohex or watersheds
   level                       INT,            -- is this applicable to all systems?
   location_uri                VARCHAR(100),
   location_id                 VARCHAR(100),
@@ -186,7 +200,8 @@ CREATE TABLE spatial_system_ref (
 );
 
 -- can this, or should this, be generalized ? with a relationship_type field
-CREATE TABLE admin_seat (
+-- combine with prec?
+CREATE TABLE historical_context (   -- admin_relationship
   id                          INT auto_increment,
   placename_id                INT NOT NULL,                  -- governed admin unit
   seat_id                     INT NOT NULL,
@@ -199,5 +214,91 @@ CREATE TABLE admin_seat (
   FOREIGN KEY placename_id REFERENCES placename.placename_id,
   FOREIGN KEY seat_id REFERENCES placename.placename_id
 );
+
+
+-- assumes multiple ids per placename - otherwise combine with main table
+-- n to 1!!!!
+CREATE TABLE src_note (
+  src_note_id                 INT auto_increment,
+
+  note_id                      VARCHAR(12),  -- a 5 digit number, sometimes prefixed with 'relig_'
+                                             -- FIXME - make sure the links in the main table go to the PK
+                                             -- find a better field name
+  compiler                     VARCHAR(64),          -- person's name, prev: nts_comp
+
+  vernacular_lang              VARCHAR(8),
+  vernacular_name              VARCHAR(100),         -- prev: nts_nmch  vc40
+
+  -- FIXME
+  -- other vernacular: simplified vs trad.   -- prev:  nts_nmft vc40
+
+  transcription_system         VARCHAR(8),
+  transcription_name           VARCHAR(100),         -- prev: nts_nmpy  tinytext
+
+  translation_lang             VARCHAR(8),
+  translation_name             VARCHAR(100),
+
+  full_text                    VARCHAR(1028),        -- prev: nts_fullnote
+
+  PRIMARY KEY  (src_note_id)
+);
+
+
+-- present location and present jurisdiction notes
+-- is there a source, attestation or other field?    FIXME
+-- is the notion of 'present' a relative one?
+CREATE TABLE present_location (
+  id                           INT auto_increment,
+  placename_id                 INT NOT NULL,
+  type                         ENUM('location', 'jurisdiction'),
+  vernacular_lang              VARCHAR(8),
+  vernacular_name              VARCHAR(100),
+
+  transcription_system         VARCHAR(8),
+  transcription_name           VARCHAR(100),
+
+  translation_lang             VARCHAR(8),
+  translation_name             VARCHAR(100),
+
+  note                         VARCHAR(512),      -- is this needed?
+
+  PRIMARY KEY id,
+  FOREIGN KEY placename_id REFERENCES placename.placename_id
+};
+
+
+-- preceeded by - immediate precedence relationship between placenames
+CREATE TABLE prec_by (
+  id                           INT auto_increment,     -- prev: pby_uniq_id
+  placename_id                 INT NOT NULL,           -- prev: pby_by_id vc12
+  pn_prec_id                   INT NOT NULL,           -- prev: pby_prev_id  vc12
+
+  begin_year                   SMALLINT,            -- prev:  pby_begyr int8
+  end_year                     SMALLINT,            -- prev:  pby_endyr int8
+
+  PRIMARY KEY  (id),
+  FOREIGN KEY placename_id REFERENCES placename.placename_id,
+  FOREIGN KEY pn_prec_id REFERENCES placename.placename_id
+);
+
+
+-- relationship between placenames: "child is partof parent"
+-- consider placename - container terminology
+CREATE TABLE partof (
+  partof_id                    INT auto_increment,  -- prev: pot_id
+  child_id                     INT NOT NULL,        -- prev: pot_child_id  vc12
+  parent_id                    INT NOT NULL,        -- prev: pot_parent_id vc12
+
+  begin_year                   SMALLINT,            -- prev:  pot_begyr int8
+  end_year                     SMALLINT,            -- prev:  pot_endyr int8
+
+  PRIMARY KEY  (id),
+  FOREIGN KEY child_id REFERENCES(placename.placename_id),
+  FOREIGN KEY parent_id REFERENCES(placename.placename_id),
+
+);
+
+CREATE INDEX partof_child_index ON partof.child_id;
+CREATE INDEX partof_parent_index ON partof.parent_id;
 
 
