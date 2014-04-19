7401468151732c27142a607e58d92b25267e23e5
W. Hays <whays@nearity.com>
2014-04-19 10:18:07 -0400

    correction for tgw-views.sql

--- tgw/tgw-views.sql
+++ tgw/tgw-views.sql
@@ -1,10 +1,11 @@
+
+CREATE VIEW v_placename AS
 SELECT pn.*, ftype.name_vn ftype_vn, ftype.name_alt ftype_alt, ftype.name_tr ftype_tr,
 ftype.name_en ftype_en, snote.src_note_ref snote_ref, snote.full_text snote_text
-FROM placename pn, ftype, snote
-WHERE pn.ftype_id = ftype.id AND pn.snote_id = snote.id
-AND pn.id = 76;
+FROM (placename pn JOIN ftype ON pn.ftype_id = ftype.id)
+LEFT JOIN snote ON pn.snote_id = snote.id;
 
+CREATE VIEW v_spelling AS
 SELECT sp.*, script.name script, trsys.name trsys
 FROM spelling sp, script, trsys
-WHERE sp.script_id = script.id AND sp.trsys_id = trsys.id
-AND sp.placename_id = 76;
+WHERE sp.script_id = script.id AND sp.trsys_id = trsys.id;
