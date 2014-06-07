bc61dc106502784b05ac71923ae35962a6624677
W. Hays <whays@nearity.com>
2014-06-07 17:19:53 -0400

    revised materialized view for search to include earliest parent, moved to separate file to allow for running separately; revised search.php to used the new materialized view

--- tgw/webservice/search.php
+++ tgw/webservice/search.php
@@ -7,9 +7,7 @@ define('FIXED_KEY_CHAR','$');
  *
  *
  */
-function search_placename($conn, $name_key, $year_key, $fmt = 'json') {
-
-
+function search_placename($conn, $name_key, $year_key, $fmt = 'json', $data_src = 'ANY', $ftype = 'ANY') {
 
   if(substr_compare($name_key, '$', -1, 1) === 0) {   //test for last char '$' to indicate no wildcard
     $name_key = substr($name_key, 0, -1);
@@ -17,16 +15,22 @@ function search_placename($conn, $name_key, $year_key, $fmt = 'json') {
     $name_key = $name_key . '%';
   }
 
-  $query = "SELECT pn.sys_id, pn.data_src, pn.name, pn.transcription, concat(pn.beg_yr, '-', pn.end_yr) years, " .
+  $query = "SELECT pn.sys_id, pn.data_src, pn.name, pn.transcription, pn.beg_yr, pn.end_yr, " .
       "pn.ftype_vn, pn.ftype_tr, " .
-      "parent.sys_id 'parent sys_id', parent.name 'parent name', parent.transcription 'parent transcription' " .
-//      "parent.beg_yr 'parent begin year', parent.end_yr 'parent end year' " .
-      "FROM mv_pn_srch pn LEFT JOIN part_of pof ON (pn.id = pof.child_id) " .
-      "LEFT JOIN mv_pn_srch parent ON (parent.id = pof.parent_id) " .
-      "WHERE pn.transcription LIKE ? ";
+      "pn.parent_sys_id 'parent sys_id', pn.parent_vn 'parent name', pn.parent_tr 'parent transcription' " .
+      "FROM mv_pn_srch pn JOIN spelling sp ON (sp.placename_id = pn.id) " .
+      "WHERE sp.written_form LIKE ? ";
 
   if ($year_key) {
       $query .= "AND (pn.beg_yr >= ? AND pn.end_yr <= ? ) ";
+  }
+
+  if ($data_src != 'ANY') {
+      $query .= "AND pn.data_src = '" . $data_src . "' ";
+  }
+
+  if ($ftype != 'ANY') {
+      $query .= "AND pn.ftype_tr = '" . $ftype . "' ";  // FIXME   LIKE ?? Chinese vs Roman ?? some official id ??
   }
 
   $query .=    "ORDER BY pn.transcription, pn.sys_id limit " . MAX_SEARCH_HITS . ";";
@@ -57,7 +61,7 @@ function search_placename($conn, $name_key, $year_key, $fmt = 'json') {
      alt_response(500);
      return;
   } else {
-
+    // workaround for lack of access to stmt->get_result method
     $pns = array();  //indexed
 
     $md = $stmt->result_metadata();
@@ -82,15 +86,13 @@ function search_placename($conn, $name_key, $year_key, $fmt = 'json') {
 
     $stmt->close();
 
-    $pns2 = reduce_to_first_parent($pns);
-
     switch($fmt) {
       case 'json':
-        search_to_json($pns2, $name_key, $year_key); break;
+        search_to_json($pns, $name_key, $year_key); break;
       case 'html':
-        search_to_html($pns2, $name_key, $year_key); break;
+        search_to_html($pns, $name_key, $year_key); break;
       case 'xml':
-        search_to_xml($pns2, $name_key, $year_key); break;
+        search_to_xml($pns, $name_key, $year_key); break;
       default:
         tlog(E_WARNING, "Invalid fmt type: " . $fmt);
         //FIXME - output?? shouldn't really get here
@@ -98,50 +100,31 @@ function search_placename($conn, $name_key, $year_key, $fmt = 'json') {
   }
 }
 
-// input:  ordered placenames
-// process:  for repeated sys_ids, filter for the first parent in each group
-// for alternative to display more than the first parent, see the function 'reduce parents' below
-function reduce_to_first_parent($pns) {
-
-  $pns3 = array();
-
-  $prev = null;
-  $max_parents = 6;      //maximum number of parents to display
-
-  foreach($pns as $pn) {
-
-    if ($pn['parent sys_id'] == null) {
-      $parent = '';
-    } else {
-      $parent =  $pn['parent name'] . " (" . $pn['parent transcription'] . ")";
-    }
-
-    $parent_count = 0;
-
-    if (($prev != null) && ($pn['sys_id'] == $prev['sys_id'])) {
-        //skip later parents
-    } else {
-
-      $pns3[] = array(
-        'sys_id'          => $pn['sys_id'],
-        'uri'             => 'http://chgis.hmdc.harvard.edu/placename/' . $pn['sys_id'],
-        'name'            => $pn['name'],
-        'transcription'   => $pn['transcription'],
-        'years'           => $pn['years'],
-        'parent'          => $parent,
-        'feature type'    => $pn['ftype_vn'] . " (" . $pn['ftype_tr'] . ")",
-        'data source'     => $pn['data_src']
-      );
-
-      $prev = $pn;  //keep a reference to this pn for the next iteration
-    }
-  }
-
-  return $pns3;
-}
-
-
 function search_to_json($pns, $name_key, $year_key) {
+
+    //reformat field display for json
+    $pns_json = [];
+
+    foreach($pns as $pn) {
+
+        if ($pn['parent sys_id'] == null) {
+          $parent_name = '';
+        } else {
+          $parent_name =  $pn['parent name'] . " (" . $pn['parent transcription'] . ")";
+        }
+
+        $pns_json[] = array(
+          'sys_id'          => $pn['sys_id'],
+          'uri'             => 'http://chgis.hmdc.harvard.edu/placename/' . $pn['sys_id'],
+          'name'            => $pn['name'],
+          'transcription'   => $pn['transcription'],
+          'years'           => $pn['beg_yr'] . " ~ " . $pn['end_yr'],
+          'parent sys_id'   => $pn['parent sys_id'],
+          'parent name'     => $parent_name,
+          'feature type'    => $pn['ftype_vn'] . " (" . $pn['ftype_tr'] . ")",
+          'data source'     => $pn['data_src']
+        );
+    }
 
     $jt = "{\n";
     $indent = '  ';
@@ -153,7 +136,7 @@ function search_to_json($pns, $name_key, $year_key) {
         . ( (count($pns) >= MAX_SEARCH_HITS) ? '  Returned more than the maximum. Please refine your search.' : ''), 1)
         .  jline('count', count($pns), 1)
 //        .  $indent . "\"hits\" : [\n"
-        .  jarray('hits', $pns, 1, true)
+        .  jarray('placenames', $pns_json, 1, true)
 //        .  $indent . "},\n"
         . "}";
 
@@ -172,97 +155,28 @@ function search_to_xml($pns, $name_key, $year_key) {
 
     foreach($pns as $pn) {
        $t .= "    <placename sys_id=\"" . $pn['sys_id'] . "\">\n"
-           . "      <uri>" . $pn['uri'] . "</uri>\n"
+           . "      <uri>" . 'http://chgis.hmdc.harvard.edu/placename/' . $pn['sys_id'] . "</uri>\n"
            . "      <name>" . $pn['name'] . "</name>\n"
            . "      <transcription>" . $pn['transcription'] . "</transcription>\n"
-           . "      <years>" . $pn['years'] . "</years>\n"
-           . "      <parent>" . $pn['parent'] . "</parent>\n"
-           . "      <feature-type>" . $pn['feature type'] . "</feature-type>\n"
-           . "      <data-source>" . $pn['data source'] . "</data-source>\n"
+           . "      <years>" . $pn['beg_yr'] . " ~ " . $pn['end_yr'] . "</years>\n";
+
+           if ($pn['parent sys_id'] == null) {
+             $t .= "      <parent />\n";
+           } else {
+             $t .= "      <parent sys_id=\"" . $pn['parent sys_id'] . "\">" . $pn['parent name']
+                . " (" . $pn['parent transcription'] . ")</parent>\n";
+           }
+
+           $t .= "      <feature-type>" . $pn['ftype_vn'] . " (" . $pn['ftype_tr'] . ")</feature-type>\n"
+           . "      <data-source>" . $pn['data_src'] . "</data-source>\n"
            . "    </placename>\n";
     }
-
 
     $t .= "  </placenames>\n"
        . "</search-results>";
 
-
-
     header('Content-Type: text/xml; charset=utf-8');
     echo $t;
 }
-
-function search_to_html($pns, $name_key, $year_key) {
-
-    $t = "<!DOCTYPE html>\n"
-      . "<html>\n"
-      . "<body>\n";
-
-     // introductory remarks here
-
-     // <div class="results">
-
-    foreach ($pns as $pn) {
-    $t .= "    <dl class=\"pn\">" . $pn['sys_id']
-       .  "        <dt class=\"pnt\"></dt><dd class=\"pnd\"></dd>\n"
-       // etc.
-       . "</dl>\n";
-    }
-
-    $t .= "</body>\n"
-       .  "</html>";
-
-    header('Content-Type: text/html; charset=utf-8');
-    echo $t;
-}
-
-// use this to capture multiple parents (NOT CURRENTLY USED CODE)
-// input:  ordered placenames
-// process:  for repeated sys_ids reformat as a single item with a compound field for multiple parents
-/*
-function reduce_parents($pns) {
-  // create new array
-  $pns2 = array();
-
-  $prev = null;
-  $max_parents = 6;      //maximum number of parents to display
-
-  foreach($pns as $pn) {
-
-    if ($pn['parent sys_id'] == null) {
-      $parent = null;
-    } else {
-      $parent = array(
-          'sys_id'          => $pn['parent sys_id'],
-          'name'            => $pn['parent name'],
-          'transcription'   => $pn['parent transcription'],
-          'years'           => $pn['parent begin year'] . " ~ " . $pn['parent end year']
-      );
-    }
-
-    $parent_count = 0;
-
-    if (($prev != null) && ($pn['sys_id'] == $prev['sys_id'])) {
-      if ($parent_count++ < $max_parents) {
-        $pns2[count($pns2) - 1]['parents'][] =   $parent;
-      }
-    } else {
-
-      $pns2[] = array(
-        'sys_id'          => $pn['sys_id'],
-        'name'            => $pn['name'],
-        'transcription'   => $pn['transcription'],
-        'years'           => $pn['years'],
-        'parents'         => ($parent == null ? array() : array($parent)),
-        'data source'     => $pn['data_src']
-      );
-
-      $prev = $pn;  //keep a reference to this pn for the next iteration
-    }               //however, cannot append to this reference (php mystery)
-  }
-
-  return $pns2;
-}
-*/
 
 ?>
