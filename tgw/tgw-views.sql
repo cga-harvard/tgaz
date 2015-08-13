f5363d5ee39d83d3eb010ba91bcfc7339f5ba084
W. Hays <whays@nearity.com>
2015-08-13 12:14:36 -0400

    in tgw-views.sql, add source_note_uri field to v_placename, fix typo in v_partof and v_precby, add new view v_children

--- tgw/tgw-views.sql
+++ tgw/tgw-views.sql
@@ -1,7 +1,7 @@
 
 CREATE OR REPLACE VIEW v_placename AS
 SELECT pn.*, ftype.name_vn ftype_vn, ftype.name_alt ftype_alt, ftype.name_tr ftype_tr,
-ftype.name_en ftype_en, snote.src_note_ref snote_ref, snote.full_text snote_text
+ftype.name_en ftype_en, snote.src_note_ref snote_ref, snote.full_text snote_text, snote.uri snote_uri
 FROM (placename pn JOIN ftype ON pn.ftype_id = ftype.id)
 LEFT JOIN snote ON pn.snote_id = snote.id;
 
@@ -18,7 +18,16 @@ FROM part_of po JOIN  placename pn ON (po.parent_id = pn.id)
 LEFT JOIN spelling sp_vn ON (pn.id = sp_vn.placename_id)
 LEFT JOIN spelling sp_tr ON (pn.id = sp_tr.placename_id)
 WHERE sp_vn.script_id > 0 AND sp_vn.default_per_type = 1
-AND sp_tr.trsys_id != 'na' AND sp_vn.default_per_type = 1;
+AND sp_tr.trsys_id != 'na' AND sp_tr.default_per_type = 1;
+
+-- outer joins are needed since spellings are not always there for a given type
+CREATE OR REPLACE VIEW v_children AS
+SELECT po.*, pn.sys_id child_sys_id, sp_vn.written_form child_vn, sp_tr.written_form child_tr
+FROM part_of po JOIN  placename pn ON (po.child_id = pn.id)
+LEFT JOIN spelling sp_vn ON (pn.id = sp_vn.placename_id)
+LEFT JOIN spelling sp_tr ON (pn.id = sp_tr.placename_id)
+WHERE sp_vn.script_id > 0 AND sp_vn.default_per_type = 1
+AND sp_tr.trsys_id != 'na' AND sp_tr.default_per_type = 1;
 
 
 CREATE OR REPLACE VIEW v_precby AS
@@ -27,7 +36,7 @@ FROM prec_by pb JOIN  placename pn ON (pb.prec_id = pn.id)
 LEFT JOIN spelling sp_vn ON (pn.id = sp_vn.placename_id)
 LEFT JOIN spelling sp_tr ON (pn.id = sp_tr.placename_id)
 WHERE sp_vn.script_id > 0 AND sp_vn.default_per_type = 1
-AND sp_tr.trsys_id != 'na' AND sp_vn.default_per_type = 1;
+AND sp_tr.trsys_id != 'na' AND sp_tr.default_per_type = 1;
 
 -- for search query
 -- CREATE OR REPLACE VIEW v_pn_srch AS
